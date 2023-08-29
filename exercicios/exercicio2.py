dce8a661c65f58abfff3852aef830c8531c84b40
letyresina <leticiaresina@gmail.com>
2023-08-29 08:38:12 -0300

    Subindo exercício 2

--- exercicios/exercicio2.py
+++ exercicios/exercicio2.py
@@ -5,3 +5,20 @@
     - Solicite os dados ao usuário.
     Percorra o dicionário e exiba o nome dos produtos com preço superior a R$ 50.00
 '''
+
+produtos = {}
+
+while len(produtos) < 5:
+    tituloProduto = input("Informe o nome do produto: ").title()
+    preco = float(input("Informe o preço do produto: "))
+    if tituloProduto in produtos:
+        print("Já existe esse produto cadastrado no sistema! Tente novamente")
+    else:
+        produtos[tituloProduto] = preco
+        print("Cadastro realizado com sucesso")
+
+print(produtos)
+
+for i in produtos.keys():
+    if produtos[i] > 50:
+        print(i)
