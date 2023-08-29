c4e58ed8d2e43fbccf074fbe94d585b92b1e0540
letyresina <leticiaresina@gmail.com>
2023-08-29 08:20:39 -0300

    feito exercício 1

---
+++ exercicios/exercicio1.py
@@ -0,0 +1,19 @@
+'''
+    Exercício 1:
+    reencha um dicionário com as informações de 5 pessoas.
+    - Utilize o cpf da passoa como chave e o nome como valor.
+    - Solicite os dados ao usuário.
+'''
+
+pessoas = {}
+
+while len(pessoas) < 5:
+    cpf = int(input("Informe o CPF (somente números!): "))
+    nome = input("Informe o nome do portador do CPF: ")
+    if cpf not in pessoas:
+        pessoas[cpf] = nome
+        print("Cadastro feito com sucesso!")
+    else:
+        print("Já existe esse CPF cadastrado no sistema! Tente novamente")
+
+print(f"{pessoas}")
